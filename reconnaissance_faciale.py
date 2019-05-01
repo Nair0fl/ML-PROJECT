8de400a73fbd25621027879b68c8c770ac86adaf
Antoine <antoinedrouard@gmail.com>
2019-05-01 22:56:10 +0200

    Amélioration qualité du code / pylint

--- reconnaissance_faciale.py
+++ reconnaissance_faciale.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 26 08:09:13 2019
-
-@author: Clément
+Script basé en partie sur un de nos exercices de reconnaissance faciale
 """
 from sklearn.datasets import fetch_olivetti_faces
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 
 class ReconnaissanceFaciale:
-    
-    """ Lance la reconnaissance faciale avec des données par défaut """
+    """
+    Classe regroupant différentes méthodes utiles pour tester la reconnaissance faciale
+    d'une photo passée en entrée par rapport à un jeux de donnée fournit.
+    """
     def test(self):
+        """ Lance la reconnaissance faciale avec des données par défaut """
         #Chargement des données
         faces = fetch_olivetti_faces()
         #Séparation data / target
@@ -24,26 +25,26 @@ class ReconnaissanceFaciale:
         #Entrainement
         self.entrainer(x_train, y_train, clf)
         #Prédiction
-        for key, x in enumerate(x_test):
-            self.reconnaitre_un_visage(clf, x, y_test[key])
-    
-    """ Entrainement sur des photos """
+        for key, current in enumerate(x_test):
+            self.reconnaitre_un_visage(clf, current, y_test[key])
+
     def entrainer(self, x_train, y_train, classifieur):
+        """ Entrainement sur des photos """
         #Entrainement du classifieur
         classifieur.fit(x_train, y_train)
         #Calcul du nombre de photos dans le jeu d'entrainement
         nb_photos = len(x_train)
         #Calcul du nombre de personnes dans le jeu d'entrainement
         personnes = []
-        for p in y_train:
-            if p not in personnes:
-                personnes.append(p)
+        for person in y_train:
+            if person not in personnes:
+                personnes.append(person)
         nb_personnes = len(personnes)
         #Affichage du nombre de photos et de personnes dans le jeu d'entrainement
-        print("Je me suis entrainé sur", nb_photos, "photos représentant le visage de", nb_personnes,"personnes")
-    
-    """ Identifier un visage """
+        print("Je me suis entrainé sur", nb_photos, "photos représentant le visage de", nb_personnes, "personnes")
+
     def reconnaitre_un_visage(self, classifieur, visage, reponse=None):
+        """ Identifier un visage """
         #Tentative de prédiction
         prediction = classifieur.predict([visage])
         #Affichage de la prédiction
@@ -51,7 +52,7 @@ class ReconnaissanceFaciale:
             if prediction == reponse:
                 print("J'ai correctement identifié", prediction[0])
             else:
-                print("Je me suis trompé, je pensais qu'il s'agissait de", prediction[0],"mais c'était", reponse)
+                print("Je me suis trompé, je pensais qu'il s'agissait de", prediction[0], "mais c'était", reponse)
         else:
             print("Je pense qu'il s'agit de", prediction[0])
         return prediction[0]
